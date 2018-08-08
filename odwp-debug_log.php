6f20e09445f65bdbda9a41efe5a0b4617df779a8
Ondřej Doněk <ondrejd@gmail.com>
2018-08-08 19:36:54 +0200

    Header of main plugin file updated.

--- odwp-debug_log.php
+++ odwp-debug_log.php
@@ -2,17 +2,37 @@
 /**
  * Plugin Name: Debug Log Viewer
  * Plugin URI: https://github.com/ondrejd/odwp-debug_log
- * Description: Small plugin aimed for developers that want easilly see their debug.log file.
+ * Description: Small plugin aimed for developers that want easily see their <code>debug.log</code> file.
  * Version: 1.0.0
  * Author: Ondřej Doněk
  * Author URI: https://ondrejd.com/
  * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
  * Requires at least: 4.7
- * Tested up to: 4.9.5
+ * Requires PHP: 7.0
+ * Tested up to: 4.9.8
  * Tags: debug,log,development
  * Donate link: https://www.paypal.me/ondrejd
  * Text Domain: odwpdl
  * Domain Path: /languages/
+ *
+ * WordPress plugin Debug Log Viewer helps developers with reviewing
+ * their `debug.log` file.
+ *
+ * Copyright (C) 2018 Ondřej Doněk
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * @author Ondřej Doněk <ondrejd@gmail.com>
  * @link https://github.com/ondrejd/odwp-debug_log for the canonical source repository
